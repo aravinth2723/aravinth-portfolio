d51a509cab68c7ed42b4d82f12aba7ca5c0ccb4d
Aravinth <aravinth666.in@gmail.com>
2025-05-29 19:46:32 +0530

    Responsive device breakpoint observer completed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  HostBinding,
+  inject,
+  OnDestroy,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { Responsive } from './services/responsive';
+import { DeviceType } from './interfaces/device-type';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +16,57 @@ import { Component } from '@angular/core';
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {}
+export class App implements OnInit, OnDestroy {
+  private responsiveService = inject(Responsive);
+  private destroy$ = new Subject<void>();
+
+  // Reactive signal for device type
+  readonly deviceType = signal<DeviceType>({
+    mobile: false,
+    tablet: false,
+    web: false,
+  });
+
+  // Bind the class device based
+  @HostBinding('class')
+  get applyDeviceClass(): string {
+    const type = this.deviceType();
+    if (type.mobile) return 'mobile';
+    if (type.tablet) return 'tablet';
+    if (type.web) return 'web';
+    return '';
+  }
+
+  ngOnInit(): void {
+    this.responsiveService.isMobile$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((isMobile) => {
+        if (isMobile) this.setDeviceType('mobile');
+      });
+
+    this.responsiveService.isTablet$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((isTablet) => {
+        if (isTablet) this.setDeviceType('tablet');
+      });
+
+    this.responsiveService.isWeb$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((isWeb) => {
+        if (isWeb) this.setDeviceType('web');
+      });
+  }
+
+  private setDeviceType(type: keyof DeviceType) {
+    this.deviceType.set({
+      mobile: type === 'mobile',
+      tablet: type === 'tablet',
+      web: type === 'web',
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
