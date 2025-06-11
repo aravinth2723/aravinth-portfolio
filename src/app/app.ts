94fe7b9e9d0c413fff9d3a068940e58fb44bf09c
Aravinth <aravinth666.in@gmail.com>
2025-06-11 13:29:30 +0530

    Unused RouterOutlet

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
+import {
+  trigger,
+  transition,
+  style,
+  animate,
+  keyframes,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss',
+  animations: [
+    trigger('fadeInSlide', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(20px)' }),
+        animate(
+          '1000ms ease-out',
+          style({ opacity: 1, transform: 'translateY(0)' })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class App {}
