99129020808736cb2c9ffc11311800dc2da8ab71
Aravinth <aravinth666.in@gmail.com>
2025-06-11 14:01:06 +0530

    Angular font-awesome added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,6 +6,8 @@ import {
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,5 +15,6 @@ export const appConfig: ApplicationConfig = {
     provideZonelessChangeDetection(),
     provideRouter(routes),
     provideAnimations(),
+    importProvidersFrom(FontAwesomeModule),
   ],
 };
